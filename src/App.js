8ed6037be85a479093f6dc57b1af464c8c75cecd
Franz Cadiente <fcadien1@my.centennialcollege.ca>
2022-10-16 22:50:19 -0400

    added header component with use-user and user-auth-listener hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import UserContext from './context/user';
+import useAuthListener from './hooks/use-auth-listener';
 
 // for lazy loading
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
 const NotFound = lazy(() => import('./pages/not-found'));
-
+const Dashboard = lazy(() => import('./pages/dashboard'));
 export default function App() {
+  const { user } = useAuthListener();
   return (
+    <UserContext.Provider value={{ user }}>
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Routes>
           <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-
-          <Route path="*" element={<NotFound />} />
+          <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+          <Route component={NotFound} />
         </Routes>
       </Suspense>
     </Router>
+    </UserContext.Provider>
   );
 }
